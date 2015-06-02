5e02094ff43a92700a30cfc8ce090cd5dcb61742
Anna Ershova <anna.ershova@gmail.com>
2015-06-01 21:28:43 -0400

    all done

--- syntax_error.rb
+++ syntax_error.rb
@@ -2,9 +2,7 @@ puts "This program has an error."
 puts "Answer what kind of error it is, what caused it, and where."
 
 def invalid
-  false
+  return false
 end
 
-1.times |valid| do
-  puts true
-end
+invalid
